6259215d59b3b617bcfc74df44a64381aca834fe
Nick Doiron <ndoiron@mapmeld.com>
2019-07-28 08:54:53 +0900

    system for checking multiple algorithms for accuracy

---
+++ better-prediction.py
@@ -0,0 +1,82 @@
+# better prediction attempts
+# focus on Grade 1b->2b
+# pip3 install scikit-learn numpy scipy
+
+import csv
+from random import shuffle
+
+import numpy as np
+from scipy import stats
+from sklearn import preprocessing, utils
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import SGDRegressor, BayesianRidge, ARDRegression, LinearRegression
+from sklearn.neighbors import KNeighborsRegressor, RadiusNeighborsRegressor
+from sklearn.svm import SVR
+# add XGBoost? LogisticRegression expects classifier?
+
+from sklearn.metrics import explained_variance_score, r2_score
+
+# load the school data
+# drop first column (school code)
+# last column is y-values
+original_x = []
+original_y = []
+
+with open('grad_1b.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    datarows = []
+    index = 0
+    for row in csv_reader:
+        index = index + 1
+        if (index > 1):
+            datarows.append(row)
+    shuffle(datarows)
+
+    for row in datarows:
+        xrow = []
+        vind = -1
+        for val in row[1:-1]:
+            vind = vind + 1
+            # avoid these always-zero columns
+            if vind not in [35, 37]:
+                xrow.append(int(val))
+
+        original_x.append(xrow)
+        original_y.append(float(row[-1]))
+
+original_x = np.array(original_x)
+scaled_x = stats.zscore(np.copy(original_x), axis=0)
+
+original_y = np.array(original_y)
+scaled_y = stats.zscore(np.copy(original_y), axis=0)
+
+dividing_line = int(0.8 * len(original_x))
+
+def predict(algo, zscoreX, zscoreY, urbanOnly):
+    # print algo, accuracy, r-squared?
+    print(algo)
+    print('with zScore on X: ' + str(zscoreX) + ' and Y: ' + str(zscoreY) + ' : urban only? ' + str(urbanOnly))
+
+    if zscoreX:
+        x = np.copy(scaled_x)
+    else:
+        x = np.copy(original_x)
+
+    if zscoreY:
+        y = np.copy(scaled_y)
+    else:
+        y = np.copy(original_y)
+
+    model = algo()
+    model.fit(x[:dividing_line], y[:dividing_line])
+    y_predict = model.predict(x[dividing_line:])
+
+    print(explained_variance_score(y[dividing_line:], y_predict))
+    print(r2_score(y[dividing_line:], y_predict))
+
+# eli5 and other libraries better than alibi for explaining?
+for algo in [RandomForestRegressor, SGDRegressor, BayesianRidge, ARDRegression, LinearRegression, KNeighborsRegressor, RadiusNeighborsRegressor, SVR]:
+    for zscoreX in [True, False]:
+        for zscoreY in [True, False]:
+            for urbanOnly in [True, False]:
+                predict(algo, zscoreX, zscoreY, urbanOnly)
--- compare_rates.py
+++ compare_rates.py
@@ -18,10 +18,8 @@ testfix2=> SELECT * FROM school_perf_2017_08 LIMIT 1;
       2017 | 08         | 13351       |                25 |                133 |     1 |        1 |                0 |                1
 """
 
-cursor.execute('DROP TABLE IF EXISTS geo_school_1b')
-cursor.execute('DROP TABLE IF EXISTS geo_school_2b')
-
 # get the schools and geo counts
+cursor.execute('DROP TABLE IF EXISTS geo_school_1b')
 cursor.execute('CREATE TABLE geo_school_1b AS (SELECT * FROM geo_schools)')
 
 # drop columns not needed for predictions
@@ -39,27 +37,23 @@ cursor.execute('UPDATE geo_school_1b SET grad_1b = \
     WHERE school_perf_2017_1b.school_code::text = geo_school_1b.school_code::text \
     LIMIT 1 \
 )')
-conn.commit()
-system('sql2csv --db ' + connection_string + ' --query \'SELECT * FROM geo_school_1b WHERE grad_1b IS NOT NULL \' > grad_1b.csv')
 
 # get 2b while we are here
-cursor.execute('CREATE TABLE geo_school_2b AS (SELECT * FROM geo_schools)')
 
 # add
-cursor.execute('ALTER TABLE geo_school_2b ADD COLUMN grad_2b FLOAT')
-cursor.execute('UPDATE geo_school_2b SET grad_2b = \
+cursor.execute('ALTER TABLE geo_school_1b ADD COLUMN grad_2b FLOAT')
+cursor.execute('UPDATE geo_school_1b SET grad_2b = \
 ( \
     SELECT (students_in_grade - temp_dropped_out - perm_dropped_out - repeated)::float \
         / students_in_grade::float \
     FROM school_perf_2017_2b \
-    WHERE school_perf_2017_2b.school_code::text = geo_school_2b.school_code::text \
+    WHERE school_perf_2017_2b.school_code::text = geo_school_1b.school_code::text \
     LIMIT 1 \
 )')
 conn.commit()
-system('sql2csv --db ' + connection_string + ' --query \'SELECT * FROM geo_school_2b WHERE grad_2b IS NOT NULL \' > grad_2b.csv')
+system('sql2csv --db ' + connection_string + ' --query \'SELECT grad_1b, grad_2b FROM geo_school_1b WHERE grad_1b IS NOT NULL AND grad_2b IS NOT NULL \' > grad_2b.csv')
 
 cursor.execute('DROP TABLE geo_school_1b')
-cursor.execute('DROP TABLE geo_school_2b')
 conn.commit()
 
 conn.close()
